65d02e37dc05943d27b07e0d6184f9cb575ee87d
Michal Zak <zako05@gmail.com>
2020-01-02 15:05:39 +0100

    add first spec

--- cypress/integration/todomvc.spec.js
+++ cypress/integration/todomvc.spec.js
@@ -1,6 +1,16 @@
 // todomvc.spec.js
 
-it('should navigate to the TodoMVC App', () => {
+it('should be able to add a new todo to the list', () => {
   // cy.visit('http://todomvc-app-for-testing.surge.sh/')
-  cy.visit('http://todomvc-app-for-testing.surge.sh/does-not-exist')
+  // delay to show up the element - pass
+  // cy.visit('http://todomvc-app-for-testing.surge.sh/?delay-new-todo=3000')
+  // dely to show up the element - fail - because delay is above 4s
+  // we need to change timeout for .get
+  cy.visit('http://todomvc-app-for-testing.surge.sh/?delay-new-todo=5000')
+  cy.get('.new-todo', {timeout: 6000}).type("Clean room{enter}")
+  // cy.visit('http://todomvc-app-for-testing.surge.sh/does-not-exist')
+  // cy.get('.new-todo').type("Clean room{enter}")
+  // cy.get('.old-todo').type("Clean room{enter}")
+  cy.get('.toggle').click()
+  cy.contains('Clear completed').click()
 })
